bbed560f16a39c645f01bde9a7857cbef48adaaf
Singlesaul <singlesaulwork@gmail.com>
2019-10-23 17:42:29 +0800

    基本页面布局

---
+++ js/main.js
@@ -0,0 +1,9 @@
+portfolioAll.onclick = function () {
+    barInner.className = 'stateOne'
+}
+portfolioFrame.onclick = function () {
+    barInner.className = 'stateTwo'
+}
+portfolioNative.onclick = function () {
+    barInner.className = 'stateThree'
+}
